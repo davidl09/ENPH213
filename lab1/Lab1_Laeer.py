430a1f540e715481c3a666c85ef4bdaa0c18faf4
David Laeer <dalaeer@gmail.com>
2024-01-15 11:11:33 -0500

    work on lab1

--- lab1/Lab1_Laeer.py
+++ lab1/Lab1_Laeer.py
@@ -32,21 +32,48 @@ print(len(numRange),numRange[0],numRange [ len(numRange)-1 ] - 2 * m.pi)
 
 #%%
 #plot derivatives of func
-for i, func in enumerate(lambdaFuncs):
-    plt.plot(numRange, [func(x) for x in numRange], label=f'Function {i+1}')
+plt.title("nth Derivatives for n := [0..3]")
+for i, f in enumerate(lambdaFuncs):
+    plt.plot(numRange, [f(x) for x in numRange], label=f'Derivative Order {i}')
     
 #Set x-axis ticks to match the values in numRange
 plt.xticks(np.linspace(0, 2 * np.pi, 5), ['0', 'π/2', 'π', '3π/2', '2π'])
+plt.legend()
+plt.xlabel("x")
 
 plt.show()
     
 #%%
+#Forward difference and central difference derivatives
 firstOrderFD = lambda f, x, h : (f(x + h) - f(x)) / h
 firstOrderCD = lambda f, x, h : (f(x + h/2) - f(x - h/2)) / h
 
-plt.plot(numRange, [lambdaFuncs[1](x) for x in numRange], label="First Order Analytical Derivative")
-plt.plot(numRange, [firstOrderFD(func, x, 0.15) for x in numRange], label="First Order FD Derivative")
-plt.plot(numRange, [firstOrderCD(func, x, 0.15) for x in numRange], label="First Order CD Derivative")
+plt.title("First Order FD, CD, and Analytical Derivatives")
 
+plt.plot(numRange, [lambdaFuncs[1](x) for x in numRange], label="Analytical")
+plt.plot(numRange, [firstOrderFD(lambdaFuncs[0], i, 0.15) for i in numRange], label="h:=0.15")
+plt.plot(numRange, [firstOrderCD(lambdaFuncs[0], i, 0.5) for i in numRange], label="h:=0.5")
+plt.legend()
+plt.xlabel("x")
+
+plt.show()
+
+#%%
+
+#array of precomputed h values
+hVals = np.array(10 ** np.linspace(-16, 0, 17, dtype=np.float64))
+plt.title("Absolute Error of Numerical CD and FD Derivatives w.r.t. h")
+
+plt.loglog(hVals, [abs(firstOrderFD(lambdaFuncs[0], 1, h_) - lambdaFuncs[1](1)) for h_ in hVals], label="FD Error")
+plt.loglog(hVals, [abs(firstOrderCD(lambdaFuncs[0], 1, h_) - lambdaFuncs[1](1)) for h_ in hVals], label="CD Error")
+plt.legend()
+plt.xlabel("h")
+plt.ylabel("Absolute Error")
+plt.show()
+
+#%%
+#Question 2
+
+nthOrderCD = lambda f, x_, h, n : f if n == 0 else firstOrderCD(f, x_, h) if n == 1 else nthOrderCD(firstOrderCD, x_, h, n - 1)
 
 
